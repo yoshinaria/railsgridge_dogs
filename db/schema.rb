fb0fea2bb4c407356b115bd4e1c065daf4b753e4
yoshinaria <yoshinariakiko@gmail.com>
2015-04-18 11:58:06 -0500

    Added Age field

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418162413) do
+ActiveRecord::Schema.define(version: 20150418164808) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
