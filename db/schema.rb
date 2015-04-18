e999ac0d34927380756ab18a8c745c0c055bd8e4
yoshinaria <yoshinariakiko@gmail.com>
2015-04-18 15:20:01 -0500

    added pictures to dogs

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150418164808) do
+ActiveRecord::Schema.define(version: 20150418200516) do
 
   create_table "dogs", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "age"
+    t.string   "picture"
   end
 
 end
