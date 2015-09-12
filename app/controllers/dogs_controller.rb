668f9ca12fc4a00bde5769d59769347b34080bf9
yoshinaria <yoshinariakiko@gmail.com>
2015-09-12 13:00:47 -0500

    initial commit

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -69,6 +69,6 @@ class DogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def dog_params
-      require(:dog).permit(:name, :age, :picture)
+      params.require(:dog).permit(:name, :age, :picture)
     end
 end
